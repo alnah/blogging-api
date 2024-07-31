b7e0dae7c6d05b94e33777032e6d1279b6dc10bf
alnah <alexis.nahan@gmail.com>
2024-07-31 08:23:43 -0300

    chore: add index for grouping validator exports

---
+++ src/validators/index.js
@@ -0,0 +1,11 @@
+const emailValidator = require("./emailValidator");
+const urlValidator = require("./urlValidator");
+const avatarValidator = require("./avatarValidator");
+const socialMediaValidator = require("./socialMediaValidator");
+
+module.exports = {
+  emailValidator,
+  urlValidator,
+  avatarValidator,
+  socialMediaValidator,
+};
