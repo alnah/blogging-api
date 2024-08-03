d84f355bd2958a04a32ddb50c0e21869a040c8bf
alnah <alexis.nahan@gmail.com>
2024-08-02 21:16:49 -0300

    feat(auth): add `responseMessageConstants` for managing responses

---
+++ src/constants/responseMessageConstants.js
@@ -0,0 +1,14 @@
+const RESPONSE_MESSAGES = {
+  SIGNED_UP: "Welcome! You have successfully signed up!",
+
+  EMAIL_VERIFIED: "Your email has been successfully verified!",
+
+  LOGGED_OUT: "You have been logged out. We hope to see you again soon!",
+
+  RESET_PASSWORD_INSTRUCTIONS:
+    "Please check your email for instructions to reset your password.",
+
+  RESET_PASSWORD: "Your password has been successfully reset!",
+};
+
+module.exports = RESPONSE_MESSAGES;
