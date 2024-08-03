a6d7ec3c41bc14b8b4e839b81429a5e267d7e901
alnah <alexis.nahan@gmail.com>
2024-08-02 21:46:49 -0300

    feat(server)!: use `authenticationRoutes` for path `/api/v1/authentication`

--- src/app.js
+++ src/app.js
@@ -3,20 +3,25 @@ require("express-async-errors");
 
 const app = require("express")();
 
-const {
-  loggingMiddleware: loggingHTTPRequests,
-  jsonParserMiddleware: jsonParser,
-  routeNotFoundMiddleware: routeNotFound,
-  errorHandlerMiddleware: errorHandler,
-} = require("./middlewares");
 const connectMongoDB = require("./database/connectMongoDB");
+const {
+  loggingHttpRequests,
+  jsonParser,
+  cookieParser,
+  routeNotFound,
+  errorHandler,
+} = require("./middlewares");
+
+const { authenticationRoutes } = require("./routes");
 
 const port = process.env.PORT || 8080;
 
-app.use(loggingHTTPRequests());
+app.use(loggingHttpRequests());
 app.use(jsonParser());
+app.use(cookieParser());
 
 app.get("/", (_, res) => res.send("<h1>Welcome!</h1>"));
+app.use("/api/v1/authentication", authenticationRoutes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
@@ -24,7 +29,7 @@ app.use(errorHandler);
 const startServer = async () => {
   try {
     await connectMongoDB();
-    app.listen(port, console.log(`Server listening on port ${port}...`));
+    app.listen(port, () => console.log(`Server listening on port ${port}...`));
   } catch (error) {
     console.error(error);
   }
