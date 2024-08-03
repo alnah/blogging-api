558bc869ca37ab26c3367641d85fdc6b138ac172
alnah <alexis.nahan@gmail.com>
2024-08-02 21:17:19 -0300

    chore: add index for grouping constant exports

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+const RESPONSE_MESSAGES = require("./responseMessageConstants");
+const ERROR_MESSAGES = require("./errorMessageConstants");
+
+module.exports = { RESPONSE_MESSAGES, ERROR_MESSAGES };
