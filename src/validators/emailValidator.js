eb6c2c7f446cd8748837a9a60e076b7ea5a455e5
alnah <alexis.nahan@gmail.com>
2024-08-02 21:45:52 -0300

    chore: remove obsolete files that have been relocated or renamed

--- src/validators/emailValidator.js
+++
@@ -1,8 +0,0 @@
-const validator = require("validator");
-
-const emailValidator = {
-  validator: validator.isEmail,
-  message: "Please enter a valid email address.",
-};
-
-module.exports = emailValidator;
--- src/validators/urlValidator.js
+++
@@ -1,8 +0,0 @@
-const validator = require("validator");
-
-const urlValidator = {
-  validator: validator.isURL,
-  message: "Please enter a valid URL.",
-};
-
-module.exports = urlValidator;
