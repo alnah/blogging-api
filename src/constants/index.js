0812cdb36dcb60492accd58c4ffeee4d71be9322
alnah <alexis.nahan@gmail.com>
2024-08-06 11:13:26 -0300

    chore: update index for grouping constant exports

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,14 @@
 const { ENV } = require("./environmentConstants");
+const { ONE_MB, THREE_MB } = require("./miscellaneousConstants");
 const RESPONSE_MESSAGES = require("./responseMessageConstants");
 const ERROR_MESSAGES = require("./errorMessageConstants");
+const SELECT_FIELDS = require("./selectFieldsConstants");
 
-module.exports = { ENV, RESPONSE_MESSAGES, ERROR_MESSAGES };
+module.exports = {
+  ENV,
+  ONE_MB,
+  THREE_MB,
+  RESPONSE_MESSAGES,
+  ERROR_MESSAGES,
+  SELECT_FIELDS,
+};
