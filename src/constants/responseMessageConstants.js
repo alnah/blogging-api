78ed7955c60d1d2f3c30da772e65a54885a698b3
alnah <alexis.nahan@gmail.com>
2024-08-06 11:10:48 -0300

    feat(user): add and sort `responseMessageConstants` alphabetically for improved handling of response messages

--- src/constants/responseMessageConstants.js
+++ src/constants/responseMessageConstants.js
@@ -1,14 +1,28 @@
 const RESPONSE_MESSAGES = {
-  SIGNED_UP: "Welcome! You have successfully signed up!",
+  DELETED_ACCOUNT:
+    "We're sorry to see you go! Your account has been successfully deleted.",
 
-  EMAIL_VERIFIED: "Your email has been successfully verified!",
+  EMAIL_UPDATED:
+    "Your email has been updated! Please verify your new email address.",
+
+  EMAIL_VERIFIED:
+    "Your email has been verified! You can now log in.",
 
   LOGGED_OUT: "You have been logged out. We hope to see you again soon!",
 
-  RESET_PASSWORD_INSTRUCTIONS:
-    "Please check your email for instructions to reset your password.",
+  PASSWORD_UPDATED: "Your password has been updated successfully!",
 
-  RESET_PASSWORD: "Your password has been successfully reset!",
+  PROFILE_UPDATED: "Your profile has been updated successfully!",
+
+  RESET_PASSWORD: "Your password has been reset successfully!",
+
+  RESET_PASSWORD_INSTRUCTIONS:
+    "Check your email for instructions to reset your password.",
+
+  SIGNED_UP:
+    "Welcome! You have successfully signed up! Please verify your account.",
+
+  USERNAME_UPDATED: "Your username has been updated successfully.",
 };
 
 module.exports = RESPONSE_MESSAGES;
