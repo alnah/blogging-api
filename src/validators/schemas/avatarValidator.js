6012375bd967dfac43183c389f03ae5a365d6783
alnah <alexis.nahan@gmail.com>
2024-08-08 10:05:42 -0300

    chore: delete `avatarValidator` as it has been replaced by `serverPathValidator`

--- src/validators/schemas/avatarValidator.js
+++
@@ -1,8 +0,0 @@
-const avatarValidator = {
-  validator: (v) => v.startsWith("./src/uploads/avatars/"),
-  message: (props) =>
-    `${props.value} is not a valid avatar path. It should start with ` +
-    "./src/uploads/avatars/",
-};
-
-module.exports = avatarValidator;
