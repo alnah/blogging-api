69a8f49dfb7d3540f5c6e4fe7375f5c1024d3b0e
alnah <alexis.nahan@gmail.com>
2024-08-08 10:04:08 -0300

    feat(server)!: setup `cloudinary.config` and use `postRoutes` for path `/api/v1/posts`

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
 const app = require("express")();
+const cloudinary = require("cloudinary").v2;
 const connectMongoDB = require("./database/connectMongoDB");
 const {
   logHttpRequests,
@@ -8,17 +9,27 @@ const {
   cookieParser,
   routeNotFound,
   errorHandler,
+  fileUploader
 } = require("./middlewares");
-const { authenticationRoutes } = require("./routes");
+const { authenticationRoutes, userRoutes, postRoutes } = require("./routes");
 
 const port = process.env.PORT || 8080;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(logHttpRequests());
 app.use(jsonParser());
 app.use(cookieParser());
+app.use(fileUploader());
 
 app.get("/", (_, res) => res.send("<h1>Welcome!</h1>"));
 app.use("/api/v1/authentication", authenticationRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
